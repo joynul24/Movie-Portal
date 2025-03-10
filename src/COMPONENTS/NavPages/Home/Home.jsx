269f2ec22adf5d21dec625d5cf9e963bc5fbce8c
joynul24 <joynula919@gmail.com>
2025-03-11 03:15:53 +0600

    added extra services section

--- src/COMPONENTS/NavPages/Home/Home.jsx
+++ src/COMPONENTS/NavPages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../Banner/Banner";
 import FeaturedMovie from "../../HomeRoute/FeaturedMovie";
+import Service from "../../HomeRoute/Service";
 
 function Home() {
   return (
     <>
       <Banner></Banner>
       <FeaturedMovie></FeaturedMovie>
+      <Service></Service>
     </>
   );
 }
